10c5d3b15e232f7ce7adc9a945caa38a512c4363
Vicent_Chen <1336754721@qq.com>
2018-04-06 11:35:59 +0800

    finish function

--- Learn/Gramma.py
+++ Learn/Gramma.py
@@ -11,7 +11,7 @@
 # Python输入的值为字符串
 # 
 # Python不支持数字与字符串相加
-# 
+# =================
 
 # ##### Output Literials #####
 """
@@ -53,7 +53,7 @@ print("Hello" + number) # OUTPUT: Hello%number%
 # Python中变量无固定类型(弱类型)
 # 
 # Python中仅会人为大写变量名当作常量
-# 
+# =================
 
 
 # ##### Types #####
@@ -107,7 +107,7 @@ print(10 // 3)
 # format格式化字符串对数字长度无要求
 # 
 # decode('utf-8', errors="ignore") 可忽略解码错误 
-# 
+# =================
 
 # ##### String #####
 """
@@ -161,6 +161,8 @@ print("Hello %% {0} X {1} = {2}".format("World", 10, 3.1415926)) # OUTPUT: Hello
 # Tuple初始化后不可修改
 # 元素获取方法与List一致
 # Tuple中仅含一个元素必须如下定义: (x, )
+# 可以用x, y = Tuple进行赋值, List亦如此
+# =================
 
 # ##### List #####
 """
@@ -195,6 +197,8 @@ print(("Vicent", "_", "Chen")[0]) # OUTPUT: Vicent
 print(("Vicent", "_", "Chen")[-3]) # OUTPUT: Vicent
 print((1)) # OUTPUT: 1
 print((1, )) # OUTPUT: (1,)
+x, y = (-0.5328330203333975, -0.8462204041751706)
+print(x, y) # OUTPUT: -0.5328330203333975 -0.8462204041751706
 """
 
 # ##### Errors #####
@@ -212,6 +216,7 @@ print((1, )) # OUTPUT: (1,)
 # if, else后记得添加':'
 #
 # int() 可用于parse整数
+# =================
 
 # ##### If #####
 """
@@ -235,8 +240,10 @@ else:
 # ================
 
 # ===== Notes =====
+# =================
 
 # ##### Loop #####
+"""
 username = ["Vicent", "_", "Chen"]
 # OUTPUT: 
 #   Vicent
@@ -257,6 +264,7 @@ while n > 0:
     n -= 1
 print(sum) # OUTPUT: 4950
 # ignore break and continue
+"""
 
 # ========================
 # ----- Dict and Set -----
@@ -266,7 +274,7 @@ print(sum) # OUTPUT: 4950
 # get()第二个参数可用于返回查找失败时的值
 # 
 # Set 自动过滤重复元素
-# 
+# =================
 
 # ##### Dict #####
 """
@@ -293,3 +301,83 @@ print(set([1, 2]) - set([2, 3])) # OUTPUT: {1}
 
 # -------------------------------------------------------------------------------------
 
+# ====================
+# ----- Function -----
+# ====================
+
+# ===== Notes =====
+# 发生错误后若不进行处理会使程序直接中断
+# Python可返回多个值
+# import不强制放在头部
+# 多次import不会报错
+#
+# 默认参数必须指向不变对象
+# 可变参数理解为Tuple
+# 为可变参数传入一个List或Tuple等价于传入一个List或Tuple作为参数，即参数个数为1
+#
+# 关键字参数在函数内部自动组装成为dict
+# 参数定义的顺序必须是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+# =================
+
+# ##### Library #####
+"""
+print(abs(-100)) # OUTPUT: 100
+print(hex(100)) # OUTPUT: 0x64
+"""
+
+# ##### Define Functinos #####
+"""
+import math
+def printHello(num):
+    if not isinstance(num, int):
+        raise TypeError("num must be int")
+    if num > 10: 
+        print("Hello")
+    else:
+        pass
+    return num
+num = int(input("Input a number"))
+print(printHello(num)) # OUTPUT: (Hello\n) %num%
+
+def multiReturn(x):
+    if not isinstance(x, (int, float)):
+        raise TypeError("X must be int or float")
+    return math.cos(x), math.sin(x), x
+print(multiReturn(num)) # OUTPUT: (%cos(num)%, %sin(num)%, %num%)
+"""
+
+# ##### Parameters #####
+"""
+import math
+def defaultValue(x = 2):
+    print(x)
+defaultValue() # OUTPUT: 2
+defaultValue(3) # OUTPUT: 3
+
+def errorDefaultValue(L = []):
+    L.append("END")
+    print(L)
+errorDefaultValue([1,2,3]) # OUTPUT: [1, 2, 3, 'END']
+errorDefaultValue() # OUTPUT: ['END']
+errorDefaultValue() # OUTPUT: ['END', 'END']
+
+def varyValue(*nums):
+    print(nums)
+
+varyValue(1,2,3) # OUTPUT: (1, 2, 3)
+varyValue((1,2,3)) # OUTPUT: ((1, 2, 3),)
+varyValue(*[2,3,4]) # OUTPUT: (2, 3, 4)
+
+def keyWordValue(**kw):
+    print(kw)
+keyWordValue(Vicent=1, Chen=3) # OUTPUT: {'Vicent': 1, 'Chen': 3}
+"""
+
+# ##### Recursive #####
+# ignore
+
+# ##### Errors #####
+# print(int("1.2345")) # OUTPUT: ValueError: invalid literal for int() with base 10: '1.2345'
+# print(printHello("10")) # OUTPUT: TypeError: num must be int
+# keyWordValue("Vicent", 1, "Chen", 3) # OUTPUT: TypeError: keyWordValue() takes 0 positional arguments but 4 were given
+# keyWordValue("Vicent"=1, "Chen"=3) # OUTPUT: SyntaxError: keyword can't be an expression
